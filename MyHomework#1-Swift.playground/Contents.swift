19e62dc692e21dd04e9cbad78328ba8f58836d4b
Homework Swift#1 <serjobikov@gmail.com>
2023-01-17 15:50:33 +0300

    Operators

--- MyHomework#1-Swift.playground/Contents.swift
+++ MyHomework#1-Swift.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+/*
 var greeting = "Hello, playground World"
 
 // count
@@ -50,7 +50,7 @@ var carCanFly: Bool = false
 
 //String
 
-var hello: String = "Hello"
+var hello: String = "Hello, "
 var world: String = "World"
 var helloWorld = hello + world
 var stringValue: String = "The \(world) is amazing"
@@ -78,6 +78,59 @@ print("Student name is \(lastStudent.name) age is \(lastStudent.age)")
 typealias Student = (name: StudentName, age: Int)
 var studentFromTypealias: Student = ("Steve", 24)
 print("Student name is \(studentFromTypealias.name) age is \(studentFromTypealias.age)")
+*/
+//Операторы +, -, %,
+var four = 2 + 2
+four = 7 - 3
+four = 2 * 2
+four = 12 / 4
+
+var six = 2 + 2 * 2
+six = 2 + (2 * 2)
+var eight = (2 + 2) * 2
+var two = 2
+eight = (2 + 2) * two
+var anotherTwo = 2
+eight = (anotherTwo + 2) * two
+
+let rectangleA = 4
+let rectangleB = 6
+let rectangleS = rectangleA * rectangleB
+var rectangleP = rectangleA + rectangleB + rectangleA + rectangleB
+rectangleP = 2 * rectangleA * rectangleB
+rectangleP = 2 * (rectangleA + rectangleB)
+
+// +=, -=, *=, /=
+var inVar = 3
+inVar = inVar + 2
+//idet posledovatelno
+inVar = 3
+inVar += 2
+inVar -= 1
+inVar *= 3
+inVar /= 6
+
+// + string
+let hello = "Hello "
+let world = " World"
+var helloWorld = hello + world
+helloWorld = "Hello " + "World"
+helloWorld = "Hello " + world
+
+helloWorld = hello
+helloWorld += world
+
+//%
+inVar = 5 % 4
+4 + 1
+inVar = 5 % 2
+2 + 2 + 1
+inVar = 5 % 3
+3 + 2
+inVar = -5 % 3
+inVar = -5 % 4
+//operator delenia po modulu
+inVar = 5 % -3
 
 //!!!!New tems  //Tuples*/
 
@@ -562,10 +615,3 @@ sub(Ihs: 1)
 sub(rhs: 1)
 sub(Ihs: 1 , rhs: 1)
 */
-
-var Apple = 12
-var Dog = 44
-print("Apple")
-print("Dog")
-    // count
-
