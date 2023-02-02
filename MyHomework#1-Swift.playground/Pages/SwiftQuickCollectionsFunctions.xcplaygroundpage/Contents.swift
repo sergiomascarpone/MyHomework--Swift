95bd44bf63e8a9349562151bc954d13433932bbb
Homework Swift#1 <serjobikov@gmail.com>
2023-02-02 12:50:06 +0300

    new commit:

--- MyHomework#1-Swift.playground/Pages/SwiftQuickCollectionsFunctions.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/SwiftQuickCollectionsFunctions.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 var greating = "Hello, playground"
 
-let intArray: [Int] = [1, 2, 3, 4, 5]
+let intArray: [Int] = [0, 1, 2, 3, 4, 5]
 
 //forEach
 for item in intArray {
@@ -18,18 +18,23 @@ for item in intArray {
         evenNumbers.append(item)
     }
 }
-
 print("Even nymbers: \(evenNumbers)")
 
 evenNumbers = intArray.filter { item in
     item % 2 == 0
 }
+
+print("Even nymbers: \(evenNumbers)")
+evenNumbers = intArray.filter { $0 % 2 == 0 }
 print("Even nymbers: \(evenNumbers)")
 
-let moreThanTwo = intArray.filter { item in
+var moreThanTwo = intArray.filter { item in
     item > 2
 }
 print("More than two items: \(moreThanTwo)")
+
+moreThanTwo = intArray.filter { $0 > 2 }
+print("More than two items: \(moreThanTwo)") //более краткий код по сравнению со строкой 31 - 34.
 
 
 //map
@@ -38,10 +43,16 @@ var uppercasedNames = names.map { name in
     name.uppercased()
 }
 print(uppercasedNames)
-let namesLegent = names.map { name in
+uppercasedNames = names.map { $0.uppercased() }
+print(uppercasedNames) //более компактная запись, в отличии строк с 42 по 45.
+
+var namesLegent = names.map { name in
     name.count
 }
 print(namesLegent)
+
+namesLegent = names.map { $0.count }
+print(namesLegent) //более компактный результат по сравнению со строкой 49 - 52.
 
 let stringsArray = intArray.map { item in
     "\(item)"
@@ -56,25 +67,34 @@ print(doubleIntArray)
 // flatMap
 let arrayOfArrays = [intArray, moreThanTwo, doubleIntArray]
 print(arrayOfArrays)
-let singleArray = arrayOfArrays.flatMap { array in
+var singleArray = arrayOfArrays.flatMap { array in
 array
 }
 print(singleArray)
 
+singleArray = arrayOfArrays.flatMap({$0})
+print(singleArray)// короткая запись в отлии со строками 70 - 73.
+
 //compactMap
 let optionalsIntsArray: [Int?] = [0, nil, 3, 5, nil, 4, 8]
-let nonoptionalInstArray = optionalsIntsArray.compactMap { item in
+var nonoptionalInstArray = optionalsIntsArray.compactMap { item in
     item
 }
 print(nonoptionalInstArray)
 
-let anotherEvenIntsArray: [Int] = intArray.compactMap { item in
+nonoptionalInstArray = optionalsIntsArray.compactMap({$0})
+print(nonoptionalInstArray)//более короткая запись в отличии со строками 80 - 83.
+
+var anotherEvenIntsArray: [Int] = intArray.compactMap { item in
     if item % 2 == 0 {
         return item
     } else {
         return nil
     }
 }
+print(anotherEvenIntsArray)
+
+anotherEvenIntsArray = intArray.compactMap({($0 % 2 == 0) ? $0 : nil })
 print(anotherEvenIntsArray)
 
 //sort
@@ -85,17 +105,35 @@ sortedSingleArray = singleArray.sorted(by: {item1, item2 in
 })
 print(sortedSingleArray)
 
+sortedSingleArray = singleArray.sorted(by: {$0 < $1})
+print(sortedSingleArray)//короткая запись по сравнению со строкой 101 - 106.
+
 let sortedNames = names.sorted { name1, name2 in
     name1 > name2
 }
 print(sortedNames)
 
 var intDictionary: [Int: String] = [0: "Zero", 1: "One", 2: "Two"]
-let sortedIntDictionary = intDictionary.sorted { keyvalue1, keyvalue2 in keyvalue1.value < keyvalue2.value
+var sortedIntDictionary = intDictionary.sorted { keyvalue1, keyvalue2 in keyvalue1.value < keyvalue2.value
 }
 print(sortedIntDictionary)
 
+sortedIntDictionary = intDictionary.sorted(by: {$0.key > $1.key})
+print(sortedIntDictionary)//более короткая запись в сравнении со строками 117 - 119.
 
+//reduce
+var summ = intArray.reduce(0) { partialResult, item in
+    partialResult + item
+}
+print("summ of \(intArray) = \(summ)")
+
+summ = intArray.reduce(0) {$0 + $1}
+print("summ of \(intArray) = \(summ)")//более короткая запись в сравнении со строкой 125 - 128.
+
+let anotherDictionaryFromArray = intArray.reduce(into: [Int: String]()) { partialResult, item in
+    partialResult[item] = "\(item)"
+}
+print(anotherDictionaryFromArray)
 
 
 
