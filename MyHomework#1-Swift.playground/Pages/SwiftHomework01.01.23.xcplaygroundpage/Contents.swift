0b00fe78a83b25495a8b9d07394c43cc223f3a84
Homework Swift#1 <serjobikov@gmail.com>
2023-02-19 17:26:28 +0300

    newCommit: HomeWork

--- MyHomework#1-Swift.playground/Pages/SwiftHomework01.01.23.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/SwiftHomework01.01.23.xcplaygroundpage/Contents.swift
@@ -655,3 +655,19 @@ func numberToString(number: Int) -> String {
 func sumMix(_ arr: [Any]) -> Int {
     return arr.reduce(0) { $0 + (Int("\($1)") ?? 0) }
 }
+
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers. Examples:
+//1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+//2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+//3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+//4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+func isDivisible(_ n: Int, _ x: Int, _ y: Int) -> Bool {
+    return n % x == 0 && n % y == 0
+}
+
+// Clock shows h hours, m minutes and s seconds after midnight. Ваша задача - написать функцию, которая возвращает время с полуночи в миллисекундах.
+func past(_ h: Int, _ m: Int, _ s: Int) -> Int {
+return s * 1000 + m * 60000 + h * 3600000
+}
+
+
