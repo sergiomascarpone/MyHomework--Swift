fddb5fae5afa6618039ad76f4e8435118b40cd1a
Homework Swift#1 <serjobikov@gmail.com>
2023-02-27 14:17:48 +0300

    newCommit:SwiftFunctions

--- MyHomework#1-Swift.playground/Pages/10.SwiftFunctions.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/10.SwiftFunctions.xcplaygroundpage/Contents.swift
@@ -1,5 +1,23 @@
-//: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var greeting = "Hello, playground"
+func calculateMoney(wallet: [Int], type: Int) -> (total: Int, count: Int) {
+    
+    var sum = 0
+    var count = 0
+
+    for value in wallet {
+        
+        if (type == nil) || (type != nil && value == type) {
+            sum += value
+            count+=1
+        }
+    }
+  return (sum, count)
+}
+
+let wallet = [100, 20, 33, 1, 4, 7, 99, 3, 3, 1, 1, 4]
+
+let money = calculateMoney(wallet: wallet, type: 3)
+
+
