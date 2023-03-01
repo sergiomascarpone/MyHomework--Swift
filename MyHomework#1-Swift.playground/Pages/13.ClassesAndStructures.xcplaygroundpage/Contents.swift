ee4c73792503f99b320b3423d0f56076c100ed83
Homework Swift#1 <serjobikov@gmail.com>
2023-03-01 16:27:20 +0300

    newCommit:ClassesAndStructure

--- MyHomework#1-Swift.playground/Pages/13.ClassesAndStructures.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/13.ClassesAndStructures.xcplaygroundpage/Contents.swift
@@ -1,5 +1,25 @@
-//: [Previous](@previous)
+// Классы
+class Dog {
+    var name = ""
+    var age = 0
+    var owner = ""
+    
+    func bark() {
+        print("\(name) bark!")
+    }
+}
 
-import Foundation
+let newDog = Dog() // создаем новый обьект
+newDog.name = "Maylo"
+newDog.age = 4
+newDog.owner = "Salie"
 
-var greeting = "Hello, playground"
+newDog.bark()
+
+let anotherDog = Dog()
+anotherDog.name = "Chappie"
+anotherDog.age = 2
+anotherDog.owner = "Peter"
+
+anotherDog.bark()
+
