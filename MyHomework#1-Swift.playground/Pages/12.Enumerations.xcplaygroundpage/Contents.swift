6957bff6b820eb18f7e6ccc65a96afb7ad2a6144
Homework Swift#1 <serjobikov@gmail.com>
2023-03-01 16:57:20 +0300

    newCommit:Enum

--- MyHomework#1-Swift.playground/Pages/12.Enumerations.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/12.Enumerations.xcplaygroundpage/Contents.swift
@@ -1,5 +1,36 @@
-//: [Previous](@previous)
+// Перечеления - enum
 
-import Foundation
+enum typeDevice{
+    case laptop(mark:markLaptop)
+    case desktopComputer
+    case tablet
+    case smartPhone
+    case smartwatch
+    
+    enum markLaptop:String{
+        case apple   = "Apple MacBook"
+        case acer    = "Acer"
+        case samsung = "Samsung"
+    }
+}
 
-var greeting = "Hello, playground"
+let device:typeDevice = .laptop(mark: .apple)
+switch device {
+case .laptop(mark: let markLaptop) where markLaptop == .apple:
+    print("Notebook \(markLaptop.rawValue)") // проверка с использованием rawValue
+case .laptop(mark: let markLaptop) where markLaptop == .samsung:
+    print("Nonebook Samsung")
+case .laptop(let mark):
+    print("Notebook \(mark.rawValue)")
+case .desktopComputer:
+    print("PC")
+case .tablet:
+    print("Tablet")
+case .smartPhone:
+    print("Smarphone")
+case .smartwatch:
+    print("SmartWatch")
+
+}
+
+
