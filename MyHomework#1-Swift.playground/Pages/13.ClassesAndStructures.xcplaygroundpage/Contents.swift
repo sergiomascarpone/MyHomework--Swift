2c922efe835698d26cff53c061d1c6d21509f05f
Homework Swift#1 <serjobikov@gmail.com>
2023-03-03 15:59:04 +0300

    newCommit:ClassesAndStructures

--- MyHomework#1-Swift.playground/Pages/13.ClassesAndStructures.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/13.ClassesAndStructures.xcplaygroundpage/Contents.swift
@@ -1,4 +1,5 @@
-// Классы
+// Классы - Classes
+/*
 class Dog {
     var name = ""
     var age = 0
@@ -22,4 +23,43 @@ anotherDog.age = 2
 anotherDog.owner = "Peter"
 
 anotherDog.bark()
+ */
+
+enum Color: String{
+    case red
+    case black
+    case green
+}
+
+class Transport {
+    var year:Int = 2000
+    var color:Color = .black
+    let numberOfSeats:Int
+    
+    init(year:Int, numbers:Int) {
+        self.year = year
+        self.numberOfSeats = numbers
+    }
+}
+
+let transport = Transport(year: 2000, numbers: 5)
+print(transport.year)
+transport.color = .red
+
+class Car: Transport {
+    let vin: String
+    var number: String?
+    var musik: Bool = false
+    
+    init(vin:String, year: Int, numbers: Int) {
+        self.vin = vin
+        super.init(year: year, numbers: numbers)
+    }
+}
+
+let car = Car(vin: "urnv33uf94", year: 2022, numbers: 4)
+
+
+
+
 
