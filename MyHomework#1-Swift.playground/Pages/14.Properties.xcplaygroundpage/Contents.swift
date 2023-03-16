d8a9b9830d1d1cd6a5a208970776c3afc8b516e7
Homework Swift#1 <serjobikov@gmail.com>
2023-03-16 13:33:58 +0300

    newCommit:Properties

--- MyHomework#1-Swift.playground/Pages/14.Properties.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/14.Properties.xcplaygroundpage/Contents.swift
@@ -2,6 +2,20 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+struct Student {
+    var firstName : String {
+        
+        willSet(newFirstName) {
+            print("will set" + newFirstName + "instead of" + firstName)
+        }
+        
+        didSet(oldFirstName) {
+            print("didSet" + firstName + "instead of" + oldFirstName)
+        }
+    }
+}
 
-//: [Next](@next)
+var student = Student(firstName: "Serjo")
+
+student.firstName
+
