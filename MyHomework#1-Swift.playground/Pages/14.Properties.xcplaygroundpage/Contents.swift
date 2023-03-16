d34e26ce1ea109fd80a2125e85c6389004bdf9f8
Homework Swift#1 <serjobikov@gmail.com>
2023-03-16 13:53:34 +0300

    Properties

--- MyHomework#1-Swift.playground/Pages/14.Properties.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/14.Properties.xcplaygroundpage/Contents.swift
@@ -5,17 +5,52 @@ import Foundation
 struct Student {
     var firstName : String {
         
-        willSet(newFirstName) {
-            print("will set" + newFirstName + "instead of" + firstName)
+        willSet {
+            print("will set " + newValue + "instead of " + firstName)
         }
         
         didSet(oldFirstName) {
-            print("didSet" + firstName + "instead of" + oldFirstName)
+            print("didSet " + firstName + "instead of " + oldFirstName)
+            
+            firstName = firstName.capitalized
+        }
+    }
+    
+    var lastName : String {
+        didSet {
+            lastName = lastName.capitalized
+        }
+    }
+    
+    var fullName : String {
+        
+        get {
+            return firstName + " " + lastName
+        }
+        set {
+            print("fullName wants to be set no " + newValue)
+            
+            let words = newValue.components(separatedBy: " ")
+            
+            if words.count > 0 {
+                firstName = words[0]
+            }
+            if words.count > 1 {
+                lastName = words[1]
+            }
         }
     }
 }
 
-var student = Student(firstName: "Serjo")
+var student = Student(firstName: "Serjo", lastName: "Mascarpone")
 
 student.firstName
+student.lastName
+student.fullName
+
+student.fullName = "Alex Varlamov"
+student.fullName
+student.firstName
+student.lastName
+student.fullName
 
