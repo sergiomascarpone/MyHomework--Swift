aecf98b92fdea397b7a2c79ba2bcb67707ee4278
Homework Swift#1 <serjobikov@gmail.com>
2023-03-21 14:45:39 +0300

    newCommit:Methods

--- MyHomework#1-Swift.playground/Pages/15.Methods.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/15.Methods.xcplaygroundpage/Contents.swift
@@ -2,6 +2,92 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+struct Point {
+    var x : Int
+    var y : Int
+    /*
+    mutating func moveByX(x: Int, byY y: Int) {
+        self.x += x
+        self.y += y
+    }*/
+    
+    mutating func moveByX(x: Int, andY: Int) {
+        self = Point(x: self.x + x, y: self.y + y)
+    }
+}
 
-//: [Next](@next)
+enum Color {
+    
+    static func numberOfElements() -> Int {
+        return 2
+    }
+    
+    case White
+    case Black
+    
+    mutating func invert() {
+        if self == Color.White {
+            self = Color.Black
+        } else {
+            self = Color.White
+        }
+    }
+    func print() {
+        if self == Color.White {
+            Swift.print("White")
+        } else {
+            Swift.print("Black")
+        }
+    }
+}
+
+var c = Color.White
+c.invert()
+c.invert()
+c.invert()
+Color.numberOfElements()
+
+func move(point: inout Point, byX x: Int, byY y: Int) -> Point {
+    point.x += x
+    point.y += y
+    return point
+}
+
+var point1 = Point(x: 1, y: 1)
+var point2 = Point(x: 3, y: 5)
+
+point1 = move(point: &point1, byX: 2, byY: 4)
+print(point1)
+point1.moveByX(x: 5, andY: 7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
