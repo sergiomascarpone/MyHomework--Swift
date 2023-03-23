77f5c58a121adb222894d0cb19568fe32d4f0327
Homework Swift#1 <serjobikov@gmail.com>
2023-03-23 12:37:08 +0300

    newCommit:Inheritance

--- MyHomework#1-Swift.playground/Pages/17.Inheritance.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/17.Inheritance.xcplaygroundpage/Contents.swift
@@ -1,7 +1,47 @@
 //: [Previous](@previous)
 
 import Foundation
+ 
+class Human {
+    
+    var firstName : String = ""
+    var lastName : String = ""
+    
+    var fullName : String {
+        return firstName + " " + lastName
+    }
+    
+    func sayHello() -> String {
+        return "Hello"
+    }
+}
 
-var greeting = "Hello, playground"
+class Student : Human {
+    override func sayHello() -> String {
+        return "sup"
+    }
+}
 
-//: [Next](@next)
+class Kid : Human {
+    override func sayHello() -> String {
+        return "agu"
+    }
+}
+
+let human = Human()
+human.firstName = "Serjo"
+human.lastName = "Mascarpone"
+human.sayHello()
+
+let student = Student()
+student.firstName = "Max"
+student.lastName = "Addams"
+student.sayHello()
+
+let kid = Kid()
+kid.firstName = "Abu"
+kid.sayHello()
+
+
+
+
