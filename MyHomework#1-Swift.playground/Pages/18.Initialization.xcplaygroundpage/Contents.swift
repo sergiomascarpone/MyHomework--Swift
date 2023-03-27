ad424f3aa033e3602f4e18f36beac37e84609516
Homework Swift#1 <serjobikov@gmail.com>
2023-03-27 12:32:41 +0300

    newCommit:Initialization

--- MyHomework#1-Swift.playground/Pages/18.Initialization.xcplaygroundpage/Contents.swift
+++ MyHomework#1-Swift.playground/Pages/18.Initialization.xcplaygroundpage/Contents.swift
@@ -2,6 +2,68 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+class Student1 {
+    var firstName: String
+    var lastName: String
+    
+    var fullName: String {
+        return firstName + " " + lastName
+    }
+    init() {
+        self.firstName = ""
+        self.lastName = ""
+    }
+}
 
-//: [Next](@next)
+class Student2 {
+    var firstName = ""
+    var lastName = ""
+}
+
+class Student3 {
+    var firstName: String
+    var lastName: String
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+let s1 = Student1()
+let s2 = Student2()
+let s3 = Student3(firstName: "A", lastName: "B")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
