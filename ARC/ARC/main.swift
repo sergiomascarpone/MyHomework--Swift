de6fe1a7f81b02fe8b1be7be33e01c97f0030d95
Homework Swift#1 <serjobikov@gmail.com>
2023-04-03 23:23:15 +0300

    newCommit:ARC

--- ARC/ARC/main.swift
+++ ARC/ARC/main.swift
@@ -2,16 +2,25 @@ var playground = true
 
 class Student {
     
-    weak var teacher : Teacher? //weak - слабая ссылка, всегда должна быть var.
+    //weak var teacher : Teacher? //weak - слабая ссылка, всегда должна быть var.
+    unowned let teacher : Teacher? //неопциональный проперти
     
     deinit {
         print("goodbye student")
+    }
+    
+    init(teacher : Teacher) {
+        self.teacher = teacher
     }
 }
 
 class Teacher {
     
-    var student : Student?
+    var student : Student!
+    
+    init() {
+        student = Student(teacher: self)
+    }
     
     deinit {
         print("goodbye teacher")
@@ -20,16 +29,45 @@ class Teacher {
 
 if playground {
     
-    var student = Student()
+    var teacher = Teacher()
     
+   /*
     if playground {
-        
-        var teacher = Teacher()
-        student.teacher = teacher
+        let student = Student(teacher: teacher)
         teacher.student = student
-    }
+    }*/
     
     print("exit playground")
 }
+
+
+var x = 10
+var y = 20
+
+class Human {
+    var name = "a"
+}
+
+var h = Human()
+
+var closure : () -> () = {
+    [x] in
+    print("\(x) \(y)")
+}
+
+var closure2 : (Int) -> Int = {[x,y] (a: Int) -> Int in
+    print("\(x) \(y) \(h.name)")
+    return a
+}
+
+closure2(1)
+
+x = 30
+y = 40
+h = Human()
+h.name = "b"
+
+
+closure2(1)
 
 print("end")
