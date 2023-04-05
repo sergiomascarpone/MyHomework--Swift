086ed5692700e0a8eb0aa8c01c91e22f257ac63f
Homework Swift#1 <serjobikov@gmail.com>
2023-04-05 13:14:58 +0300

    newCommit:OptionalChaining

--- MyHomework-Swift.playground/Pages/21.Optional Chaining.xcplaygroundpage/Contents.swift
+++ MyHomework-Swift.playground/Pages/21.Optional Chaining.xcplaygroundpage/Contents.swift
@@ -38,8 +38,30 @@ p.house!
 
 p.house!.garage!.size
 
+//optional binding
+if let house = p.house {
+    if let garage = house.garage {
+        garage.size
+    }
+}
 
+if let size = p.house?.garage?.size {
+    size
+}
 
+//optional chaining
+p.house?.garage?.size //цепочка может оборваться в любом опциональном проперти, а результат этой цепочки всегда будет опциональным значением.
+p.house?.garage?.size = 3
+
+if (p.house?.garage?.size = 3) != nil {
+    print("upgrade!")
+} else {
+    print("failuree!")
+}
+
+if let size = p.house?.garage?.size {
+    size
+}
 
 
 
