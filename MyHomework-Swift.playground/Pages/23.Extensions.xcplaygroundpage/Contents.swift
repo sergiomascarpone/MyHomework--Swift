c5f6b6831989583160539eeaaa5fd9b59ecf02ce
Homework Swift#1 <serjobikov@gmail.com>
2023-04-06 17:14:27 +0300

    newCommit:Extensions

--- MyHomework-Swift.playground/Pages/23.Extensions.xcplaygroundpage/Contents.swift
+++ MyHomework-Swift.playground/Pages/23.Extensions.xcplaygroundpage/Contents.swift
@@ -16,9 +16,56 @@ extension Int {
     var evenOrOdd: EvenOrOdd {
         return isEven ? .Even : .Odd
     }
+    
+    func pow(value: Int) -> Int {
+        
+        var temp = self
+        for _ in 1..<value {
+            temp *= self
+        }
+        return temp
+    }
+    
+    mutating func powTo(value: Int) {
+        self = pow(value: value)
+    }
+    
+    var binaryString: String {
+        var result = ""
+        for i in 0..<8 {
+            result = String(self & (1 << i) > 0) + result
+        }
+        return result
+    }
+    
 }
 
-let a = 5
+extension String {
+    init(_ value: Bool) {
+        self.init(value ? 1 : 0)
+    }
+    
+    subscript(start: Int, lenght: Int) -> String {
+        let start = advance(self.startIndex, start)
+        let end   = advance(start, lenght)
+        let range = Range(start: start, end: end)
+        return self[range]
+    }
+    
+    
+}
+
+
+extension Int.EvenOrOdd {
+    var string: String {
+        switch self {
+        case .Even: return "even"
+        case .Odd: return "odd"
+        }
+    }
+}
+
+var a = 5
 
 if a % 2 != 0 {
     print("a")
@@ -28,11 +75,15 @@ if a.isOdd {
     print("a")
 }
 
+
 a.evenOrOdd
+a
+a.pow(value: 4)
+a.powTo(value: 5)
+a = 5
+a.binaryString
 
-
-
-
+let s = "Hello, World!"
 
 
 
