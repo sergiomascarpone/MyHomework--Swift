63293f629083691b77ce427594f9b3d3a6f6a3f7
Homework Swift#1 <serjobikov@gmail.com>
2023-04-10 13:54:04 +0300

    newCommir:OperatorFunctions

---
+++ MyHomework-Swift.playground/Pages/25.Operator Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,81 @@
+struct Point {
+    var x: Int
+    var y: Int
+}
+
+var p1 = Point(x: 2, y: 3)
+var p2 = Point(x: 3, y: 5)
+
+func + (a: Point, b: Point) -> Point {
+    return Point(x: a.x + b.x, y: a.y + b.y)
+}
+
+func * (a: Point, b: Point) -> Point {
+    return Point(x: a.x * b.x, y: a.y * b.y)
+}
+
+p1 + p2
+p1
+p2
+
+p1 = p1 + p2
+p1
+p2
+
+//p1 += p2
+//изменяем само значение переменной
+func += (a: inout Point, b: Point) {
+    a = a + b
+}
+
+p1
+p2
+p1 += p2
+p1
+
+func == (a: Point, b: Point) -> Bool {
+    return a.x == b.x && a.y == b.y
+}
+p1 == p2
+p1 == p1
+
+//пергрузка префиксных и постфиксных операторов
+
+var i = 5
+i
+
+prefix func ++ ( a: inout Point) -> Point {
+    a.x += 1
+    a.y += 1
+    return a
+}
+
+postfix func ++ ( a: inout Point) -> Point {
+    let b = a
+    ++a
+    return b
+}
+
+p1 = Point(x: 2, y: 3)
+p2 = Point(x: 3, y: 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
