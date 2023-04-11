ad89e5748dcf8b0470c079fdb0a600270274edee
Homework Swift#1 <serjobikov@gmail.com>
2023-04-11 11:35:19 +0300

    newCommit:Protocols-Part1

--- MyHomework-Swift.playground/Pages/26.Protocols - part1.xcplaygroundpage/Contents.swift
+++ MyHomework-Swift.playground/Pages/26.Protocols - part1.xcplaygroundpage/Contents.swift
@@ -1,3 +1,62 @@
-import Foundation
+class Student {
+    
+    var firstSname: String
+    var lastName: String
+    
+    var fullName: String {
+        return firstSname + " " + lastName
+    }
+    
+    init(firstName: String, lastName: String) {
+        self.firstSname = firstName
+        self.lastName = lastName
+    }
+}
 
-var greeting = "Hello, playground"
+class Cow {
+    var name: String?
+}
+
+struct Grass {
+    var type: String
+}
+
+let student1 = Student(firstName: "Bob", lastName: "Saddy")
+let student2 = Student(firstName: "Jim", lastName: "Aklson")
+let student3 = Student(firstName: "Billy", lastName: "Badson")
+
+let cow1 = Cow()
+cow1.name = "Murka"
+let cow2 = Cow()
+
+let grass1 = Grass(type: "Bermuda")
+let grass2 = Grass(type: "Marshall")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
