14aa63d95c28a67493e95dc9e6835c40dea74394
Serjo Bykov <serjobikov@gmail.com>
2023-04-27 14:31:57 +0300

    newCommit:Initialization

--- MyHomework-Swift.playground/Pages/18.Initialization.xcplaygroundpage/Contents.swift
+++ MyHomework-Swift.playground/Pages/18.Initialization.xcplaygroundpage/Contents.swift
@@ -6,8 +6,8 @@ class Student1 {
         return firstName + " " + lastName
     }
     init() {
-        self.firstName = ""
-        self.lastName = ""
+        firstName = ""
+        lastName = ""
     }
 }
 
@@ -17,6 +17,7 @@ class Student2 {
 }
 
 class Student3 {
+    
     var firstName: String
     var lastName: String
     
@@ -26,18 +27,87 @@ class Student3 {
     }
 }
 
+class Student4 {
+    var middleName: String?
+}
+
+class Student5 {
+    let maxAge: Int
+    
+    init() {
+        maxAge = 100
+    }
+}
+
+/*
+class Student6: Student5 {
+    override init() {
+        super.maxAge =110
+    }
+}
+ */
+
+struct Student7 {
+    var firstName: String
+    var lastNAme: String
+    
+    init() {
+        firstName = ""
+        lastNAme = ""
+    }
+    
+    init(firstName: String, lastNAme: String) {
+        self.firstName = firstName
+        self.lastNAme = lastNAme
+    }
+}
+
 let s1 = Student1()
 let s2 = Student2()
 let s3 = Student3(firstName: "a", lastName: "b")
+let s5 = Student5()
+let s7 = Student7(firstName: "a", lastNAme: "b")
 
+class Human {
+    var weight: Int
+    var age: Int
+    
+    init(weight: Int, age: Int) {
+        self.weight = weight
+        self.age = age
+    }
+    //конвинионс конструктор
+    convenience init(age: Int) {
+        self.init(weight: 0, age: age)
+    }
+    
+    convenience init(weight: Int) {
+        self.init(weight: weight, age: 0)
+    }
+    
+    convenience init() {
+        self.init(weight: 0)
+    }
+    
+    func test() {}
+}
 
+let h1 = Human(weight: 70, age: 25)
+let h2 = Human(age: 25)
+let h3 = Human()
 
-
-
-
-
-
-
+class Student: Human {
+    
+    var firstName: String
+    var lastName: String
+    
+    init(firstName: String, lastNAme: String) {
+        self.firstName = firstName
+        self.lastName = lastNAme
+        //designated initializator родительского класса
+        super.init(weight: 0, age: 0)
+    }
+}
 
 
 
