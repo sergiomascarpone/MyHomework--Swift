611c0338e2515e37d0b72666cdf78d68228bb3e5
Serjo Bykov <serjobikov@gmail.com>
2023-04-27 13:45:47 +0300

    newCommit:Initialization

--- MyHomework-Swift.playground/Pages/18.Initialization.xcplaygroundpage/Contents.swift
+++ MyHomework-Swift.playground/Pages/18.Initialization.xcplaygroundpage/Contents.swift
@@ -1,12 +1,34 @@
-import Foundation
 
+class Student1 {
+    var firstName: String = ""
+    var lastName: String = ""
+    var fullName: String {
+        return firstName + " " + lastName
+    }
+    init() {
+        self.firstName = ""
+        self.lastName = ""
+    }
+}
 
+class Student2 {
+    var firstName = ""
+    var lastName = ""
+}
 
+class Student3 {
+    var firstName: String
+    var lastName: String
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
 
-
-
-
-
+let s1 = Student1()
+let s2 = Student2()
+let s3 = Student3(firstName: "a", lastName: "b")
 
 
 
