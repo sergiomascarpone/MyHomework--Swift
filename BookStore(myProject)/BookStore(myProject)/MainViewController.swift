c5368b87e5b19a46f3688153b36903cb0cf7a34b
Serjo Bykov <serjobikov@gmail.com>
2023-05-10 19:17:18 +0300

    newCommit:TitleLabel

--- BookStore(myProject)/BookStore(myProject)/MainViewController.swift
+++ BookStore(myProject)/BookStore(myProject)/MainViewController.swift
@@ -9,16 +9,27 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    let titleLabel: UILabel = { //Создание и размещение вью
+    
+    /// Создание и размещение вью
+    let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "Book store"
-        label.textColor = .black
+        label.textColor = .white
         label.font = UIFont(name: "Avenir Next Bold", size: 40)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-
+    
+    /// Создание и размещение imageView
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "viewLogo")
+        imageView.contentMode = .scaleAspectFill // изображение на всю вью
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,9 +38,12 @@ class MainViewController: UIViewController {
       
     }
     
-    func setupView() { //Настройка обьектов
+    
+    /// Настройка обьектов
+    func setupView() {
         view.backgroundColor = .gray //Цвет всей вью
         
+        view.addSubview(imageView)
         view.addSubview(titleLabel)
     }
 
@@ -37,12 +51,19 @@ class MainViewController: UIViewController {
 
 extension MainViewController {
     
-    func setConstraints() { //Расположение обьектов
+    
+    /// Расположение обьектов
+    func setConstraints() {
         NSLayoutConstraint.activate([
+            
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 260),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 260),
+            
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-    
-    
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor), //расположение текста по центру по оси Х
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50)
     
     
     
