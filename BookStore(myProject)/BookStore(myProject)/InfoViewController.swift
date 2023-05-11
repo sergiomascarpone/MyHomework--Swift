b920724c761ed6176f2e14fafc3bb7d35e6e5bc2
Serjo Bykov <serjobikov@gmail.com>
2023-05-11 16:22:29 +0300

    newCommit:Button

---
+++ BookStore(myProject)/BookStore(myProject)/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  BookStore(myProject)
+//
+//  Created by Sergio Mascarpone on 11.05.23.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    
+  }
+}
--- BookStore(myProject)/BookStore(myProject)/MainViewController.swift
+++ BookStore(myProject)/BookStore(myProject)/MainViewController.swift
@@ -19,7 +19,7 @@ class MainViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+   
     
     /// Создание и размещение imageView
     let imageView: UIImageView = {
@@ -30,14 +30,25 @@ class MainViewController: UIViewController {
         return imageView
     }()
     
+    
+    /// Создание и размещение кнопки
+    let springButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .darkText
+        button.setTitle("Let`s go!", for: .normal)
+        button.tintColor = .white
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.layer.cornerRadius = 20
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
         setConstraints()
-      
     }
-    
     
     /// Настройка обьектов
     func setupView() {
@@ -45,12 +56,11 @@ class MainViewController: UIViewController {
         
         view.addSubview(imageView)
         view.addSubview(titleLabel)
+        view.addSubview(springButton)
     }
-
 }
 
 extension MainViewController {
-    
     
     /// Расположение обьектов
     func setConstraints() {
@@ -63,11 +73,14 @@ extension MainViewController {
             
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor), //расположение текста по центру по оси Х
-            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50)
-    
-    
-    
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
+            
+            springButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 700),
+            springButton.heightAnchor.constraint(equalToConstant: 50),
+            springButton.widthAnchor.constraint(equalToConstant: 240),
+            springButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0)
     
     ])
+      
   }
 }
