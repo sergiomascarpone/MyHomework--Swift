c6416135da013f6740e2abd5b5d88712b5e70801
Serjo Bykov <serjobikov@gmail.com>
2023-05-22 16:49:47 +0300

    newCommit:AddRegistrationView

--- BookStore(myProject)/BookStore(myProject)/EmailView.swift
+++ BookStore(myProject)/BookStore(myProject)/EmailView.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class EmailView: UIViewController {
     
+    var regView: UIImageView {
+        let regView = UIImageView()
+        regView.translatesAutoresizingMaskIntoConstraints = false
+        regView.contentMode = .scaleAspectFill
+        return regView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupRegView()
     }
     
+    
+    func setupRegView() {
+        view.backgroundColor = .white
+        
+        
+        
+    }
 }
