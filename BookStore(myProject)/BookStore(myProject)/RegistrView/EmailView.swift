186f4ba369c4112f4fff5e47016c94712e53a85d
Serjo Bykov <serjobikov@gmail.com>
2023-05-24 15:37:24 +0300

    newCommit:MYProject

--- BookStore(myProject)/BookStore(myProject)/RegistrView/EmailView.swift
+++ BookStore(myProject)/BookStore(myProject)/RegistrView/EmailView.swift
@@ -8,85 +8,12 @@
 import UIKit
 
 class EmailView: UIViewController {
-//
-//    var regView: UIImageView {
-//        let regView = UIImageView()
-//        regView.translatesAutoresizingMaskIntoConstraints = false
-//        regView.contentMode = .scaleAspectFill
-//        return regView
-//    }
-//
-//    var emailTextField: UITextField {
-//        let textField = UITextField()
-//        textField.borderStyle = .roundedRect
-//        textField.placeholder = "Enter email"
-//        textField.backgroundColor = .black
-//        textField.font = UIFont(name: "AmericanTypewriter", size: 40)
-//        return textField
-//    }
-//
-////    var passwordTextField: UITextField {
-////        let textField1 = UITextField()
-////        textField1.borderStyle = .roundedRect
-////        textField1.placeholder = "Enter password"
-////        textField1.backgroundColor = .black
-////        return textField1
-////    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        setupRegView()
-//        setupEmailTextField()
-////        setupPasswordTextField()
-//
-//    }
-//
-//    func setupRegView() {
-//        view.backgroundColor = .gray
-//    }
-//
-//    func setupEmailTextField() {
-//        view.addSubview(emailTextField)
-//        print("lookoko")
-//    }
-//
-//
-////    func setupPasswordTextField() {
-////        view.addSubview(passwordTestField)
-////        print("okokolkl")
-////    }
-//}
-
-
-//
-//  registrationView.swift
-//  BookStore(myProject)
-//
-//  Created by Sergio Mascarpone on 12.05.23.
-//
-
-//import UIKit
-//
-//class registrationView: UIViewController {
-    
-//    /// Создание и размещение Label
-//    let titleLabel: UILabel = {
-//        let label = UILabel()
-//        label.text = "Book store"
-//        label.textColor = .white
-//        label.font = UIFont(name: "AmericanTypewriter", size: 40)
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-    
+  
     //создали imageView
     let imageView: UIView = {
         let imageView = UIView()
-        imageView.backgroundColor = .cyan
         return imageView
     }()
-    
-//    private var emailNameButtonContraint: NSLayoutConstraint!
     
     
     override func viewDidLoad() {
@@ -113,27 +40,8 @@ class EmailView: UIViewController {
     fileprivate func  setupImageView() {
 
         view.addSubview(imageView)
-        imageView.backgroundColor = .cyan
 
-        view.addSubview(imageView)
-
+        
     }
-   
+
 }
-//extension EmailView {
-//
-//
-//    func setConstraints() {
-//
-//
-//        NSLayoutConstraint.activate([
-//
-//            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-//            titleLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-//            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0),
-//            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0)
-//
-//        ])
-//    }
-//
-//}
