6322a7ff3ec6dc691e5320e323f6391ff94580f7
Serjo Bykov <serjobikov@gmail.com>
2023-05-29 16:25:04 +0300

    newCommit

--- MyHomework#1-Swift.playground/Pages/SwiftLineInterpolation.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-let temperature = 12
-let windSpeed = 5
-
-let weatherDiscription = "Temperature ic about 12`C above zero wind speed is about 5 m/s"
-let weatherDiscriptionI = "Temperature is about \(temperature)`C above zero and wind speed is about \(windSpeed) m/s"
-
-let string = "\(1 + 2)" + "\(2 + 3)"
---
+++ gesterView/gesterView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gesterView
+//
+//  Created by Sergio Mascarpone on 29.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+//kdjfkdjskfjkdsfs
+    //kkdjsfdjksfljdslfjkldslfsd
+
+}
+
