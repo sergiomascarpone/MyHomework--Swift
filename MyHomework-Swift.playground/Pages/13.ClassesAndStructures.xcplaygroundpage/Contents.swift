8a87f17cbb26d6c24d48fa166912ac1509590864
Serjo Bykov <serjobikov@gmail.com>
2023-05-29 13:09:06 +0300

    newCommit:DelegateHW

--- MyHomework-Swift.playground/Pages/13.ClassesAndStructures.xcplaygroundpage/Contents.swift
+++ MyHomework-Swift.playground/Pages/13.ClassesAndStructures.xcplaygroundpage/Contents.swift
@@ -1,17 +1,43 @@
-// Классы - Classes
+import UIKit
 
-struct MyClass {
-    var radius:  Double = 4.5
+var str = "Hello, playground"
+
+class Human {
+    func feed() {
+        print("feed")
+    }
     
-    mutating func increaseRadius() {
-        self.radius += 1
-//        print("temp \(radius * 3.12 * 2)")
+    func increaseSalary() {
+        print("increaseSalary")
+    }
+    
+    var pat: Animal?
+}
+
+class Animal {
+    var owner: Human?
+    
+    func requestfood() {
+        owner?.feed()
+        owner?.increaseSalary()
     }
 }
 
-var ff = MyClass()
-ff.increaseRadius()
-print(ff.radius)
+
+// Классы - Classes
+//
+//struct MyClass {
+//    var radius:  Double = 4.5
+//
+//    mutating func increaseRadius() {
+//        self.radius += 1
+////        print("temp \(radius * 3.12 * 2)")
+//    }
+//}
+//
+//var ff = MyClass()
+//ff.increaseRadius()
+//print(ff.radius)
 
 
 /*
@@ -89,164 +115,9 @@ car.sound()
 */
 
 // Structures - Struct, Value Type VS Referense Type
-struct Resolution {
-    var width = 0
-    var height = 0
-
-}
-
-let constantStruct = Resolution()
-//constantStruct.width = 14 - не можем, так как это константа.
-var varStruct = Resolution()
-varStruct.width = 12
-
-class ViewMode {
-    var frameRate = 0.0
-    var name: String?
-}
-
-let constantClass = ViewMode()
-constantClass.name = "Ludmila"
-var varClass = constantClass
-varClass.name = "Ira"
-
-print(constantClass.name)
-
-struct FixedLenghtRange {
-    var firstValue: Int = 1
-    let lenght: Int = 2
-    
-    var compProperty: Int {
-        get {
-            return 4
-        }
-        set(newNumber) {
-            firstValue = newNumber
-            print(newNumber)
-        }
-    }
-}
-
-var lenghtClass = FixedLenghtRange()
-lenghtClass.compProperty
-lenghtClass.compProperty = 15
+//
 
 
-
-
-struct Car{
-    let numberOfSeats: Int
-    let year: Int
-    var mileage: Int
-    var isCrash: Bool = false
-    
-    mutating func crash() {
-        print("Crash")
-        self.isCrash = true
-    }
-}
-
-class CarClass {
-    let numberOfSeats: Int
-    let year: Int
-    var mileage: Int
-    var isCrash: Bool = false
-    
-    init(numberOfSeats: Int, year: Int, mileage: Int) {
-    self.numberOfSeats = numberOfSeats
-    self.year = year
-    self.mileage = mileage
-    }
-}
-
-var car = Car(numberOfSeats: 2, year: 2022, mileage: 0)
-car.mileage = 1200
-
-var car2 = car
-
-car.mileage = 1000
-car.mileage = 3000
-//print(car.mileage)
-//print(car2.mileage)
-
-let carClass = CarClass(numberOfSeats: 2, year: 2022, mileage: 0)
-carClass.mileage = 2100
-
-let carClass2 = carClass
-
-carClass.mileage = 1200
-
-print(carClass.mileage)
-print(carClass2.mileage)
-
-func changeMileage(car: CarClass, newMileage: Int) {
-    car.mileage = newMileage
-}
-
-func changeMileage(car: Car, newMileage: Int) -> Car {
-    var carInside = car
-    carInside.mileage = newMileage
-    return carInside
-}
-
-//changeMileage(car: carClass, newMileage: 5000)
-//print(carClass.mileage)
-
-car = changeMileage(car: car, newMileage: 6000)
-print(car.mileage)
-
-
-// 12.Homework - Structures and Classes
-// 1. Создайте струĸтуру студент. Добавьте свойства: имя, фамилия, год рождения, средний бал. Создайте несĸольĸо эĸземпляров этой струĸтуры и заполните их данными. Положите их всех в массив (журнал).
-struct Student {
-    var firstName: String
-    var lastName: String
-    let dateOfBirth: String
-    var averegeScore = 0.0
-}
-var aStudent = Student(firstName: "Serjo", lastName: "Mascarpone", dateOfBirth: "01.04.1993", averegeScore: 4)
-var bStudent = Student(firstName: "Sabina", lastName: "Olegovna", dateOfBirth: "12.04.2000", averegeScore: 4.8)
-var cStudent = Student(firstName: "Olga", lastName: "Kudelich", dateOfBirth: "01.04.1998", averegeScore: 5)
-var dStudent = Student(firstName: "Galina", lastName: "Kudelich", dateOfBirth: "03.06.1993", averegeScore: 5)
-
-var jurnal = [aStudent, bStudent, cStudent]
-// 2. Напишите фунĸцию, ĸоторая принимает массив студентов и выводит в ĸонсоль данные ĸаждого. Перед выводом ĸаждого студента добавляйте порядĸовый номер в “журнале”, начиная с 1.
-func printJurnal (_ jurnal: [Student]) {
-    for (index,student) in jurnal.enumerated() {
-        print("\(index + 1). Name: \(student.firstName) \(student.lastName), Date of birth: \(student.dateOfBirth), Average score: \(student.averegeScore)")
-    }
-    print()
-}
-printJurnal(jurnal)
-// 3. С помощью фунĸции sorted отсортируйте массив по среднему баллу, по убыванию и распечатайте “журнал”.
-printJurnal(jurnal.sorted {
-    $0.averegeScore > $1.averegeScore
-})
-// 4. Отсортируйте теперь массив по фамилии (по возрастанию), причем если фамилии одинаĸовые, а вы сделайте таĸ чтобы таĸое произошло, то сравниваются по имени. Распечатайте “журнал”.
-var fourthStudent = dStudent
-fourthStudent.firstName = "Galina"
-jurnal.append(fourthStudent)
-printJurnal(jurnal)
-
-var jurnalSortByName = jurnal.sorted
-{"\($0.lastName)\($0.firstName)" < "\($1.lastName)\($1.firstName)"}
-
-printJurnal(jurnalSortByName)
-// 5. Создайте переменную и присвойте ей ваш существующий массив. Измените в нем данные всех студентов. Изменится ли первый массив? Распечатайте оба массива.
-var copyJurnal = jurnal
-
-func changeStudents(_ student: inout Student) {
-    student.firstName = student.firstName.uppercased()
-    student.lastName = student.lastName.uppercased()
-    student.lastName.uppercased()
-    student.averegeScore = student.averegeScore.nextDown
-}
- 
-for i in 0..<copyJurnal.count {
-    changeStudents(&copyJurnal[i])
-}
-printJurnal(jurnal)
-printJurnal(copyJurnal)
 // 6. Теперь проделайте все тоже самое, но не для струĸтуры Студент, а для ĸласса. Каĸой результат в 5м задании? Что изменилось и почему?
 
 
