f74518e5a41bb58da7ae34ed16e9d20dd60f21d1
Serjo Bykov <serjobikov@gmail.com>
2023-07-08 20:29:30 +0300

    Initial Commit

---
+++ BookStore(MyPetProject)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookStore(MyPetProject)
+//
+//  Created by Sergio Mascarpone on 8.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
